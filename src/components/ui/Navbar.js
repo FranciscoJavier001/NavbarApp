5805ebbccf1eb082903bd0f9163a6fa9d8ca8d44
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-07-24 17:38:08 -0500

    Navbar Component

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
+import { Link, NavLink, useHistory } from 'react-router-dom'
 import { AuthContext } from '../../auth/AuthContext'
 import { types } from '../../types/types'
 
 export const Navbar = () => {
 
     const context = useContext(AuthContext)
+    // const context.user.name = useContext(AuthContext)
     // const {user:{name}, dispatch} = useContext(AuthContext)
     const history = useHistory()
 
@@ -18,6 +20,72 @@ export const Navbar = () => {
     }
 
   return (
-    <div></div>
+    <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+
+      <link
+      className='navbar-brand'
+      to="/">
+        NavbarApp
+      </link>
+
+      <div className='navbar-collapse'>
+        <div className='navbar-nav'>
+
+          <NavLink
+            activeClassName="active"
+            className="nav-item nav-link"
+            exact
+            to="/firstcreen"
+          >
+            FirstScreen
+          </NavLink>
+
+          <NavLink
+            activeClassName="active"
+            className="nav-item nav-link"
+            exact
+            to="/secondcreen"
+          >
+            SecondScreen
+          </NavLink>
+
+          <NavLink
+            activeClassName="active"
+            className="nav-item nav-link"
+            exact
+            to="/thirdScreen"
+          >
+            ThirdScreen
+          </NavLink>
+
+          <NavLink
+            activeClassName="active"
+            className="nav-item nav-link"
+            exact
+            to="/search"
+          >
+            SearchScreen
+          </NavLink>
+
+        </div>
+      </div>
+
+      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
+        <ul className='navbar-nav ml-auto'>
+
+          <span className='nav-item nav-link text-info'>
+            {context.user.name}
+            {/* {name} */}
+          </span>
+
+          <button
+          className='nav-item nav-link btn'
+          onClick={handleLogout}>
+            Logout
+          </button>
+          
+        </ul>
+      </div>
+    </nav>
   )
 }
